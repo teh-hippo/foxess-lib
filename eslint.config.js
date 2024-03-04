27d15e4e575b8ab37253da118b5f0b029cac2379
Teh Hippo <hippo@noemail.org>
2024-03-04 22:02:59 +1100

    v1

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+  ...tseslint.config(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    eslint.configs.recommended,
+    ...tseslint.configs.stylisticTypeChecked,
+    ...tseslint.configs.strictTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          project: true
+        }
+      }
+    }
+  ),
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: "double",
+    commaDangle: "never",
+    quoteProps: "as-needed",
+    arrowParens: false,
+    blockSpacing: true,
+    braceStyle: "1tbs",
+    flat: true,
+    semi: true
+  })
+];
