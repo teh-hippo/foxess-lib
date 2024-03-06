05502f703e1d4e53d672947e3c1f133fda4096f5
Teh Hippo <hippo@noemail.org>
2024-03-06 17:23:26 +1100

    Further optimisations to the package

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  splitting: true,
+  splitting: false,
   clean: true,
   dts: true,
   cjsInterop: true,
